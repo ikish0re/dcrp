945debfe7d76cb478c4e22564fb595781ac1d14f
democracyroleplay <democracyroleplay@gmail.com>
2023-10-27 16:06:53 +0530

    git changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,8 +3,8 @@ import '../css/Footer.css'
 
 export const Footer = () => {
   return (
-    <footer class="copyright-footer">
-        <div class="container">
+    <footer className="copyright-footer">
+        <div className="footer-container">
             <p>&copy; 2023 Democracy Roleplay. All Rights Reserved.</p>
         </div>
     </footer>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -12,7 +12,6 @@ const Home = () => {
         subTitle={"Roleplay"}
         description={"Welcome to Democracy Role Play! Here, your decisions have real impact and your imagination knows no limits. Engage in democratic debates, forge alliances, and shape the future. Join us on a thrilling journey where your words can change history. Enjoy your stay and have unforgettable adventures in our democratic world!🌟🗳️🌍"}
       />
-      <img className='logo' src={logo} />
       <img className='greenBoy' src={pic1} />
       <div className='homeButtons'>
         <StartButton btnType={'homeBtn'} description={'Play With us'} />
